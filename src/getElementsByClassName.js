7b051aa2dec0506c26234bf3c0c588decec187ee
dh <dh@dhfromkorea.com>
2014-06-02 12:49:57 -0700

    done the getElementsByClass (pair programming)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,20 +6,39 @@
 // But instead we're going to implement it from scratch:
 
 
-var getElementsByClassName = function(className, start_node) {
-    var result = [];
+var getElementsByClassName = function(className) {
 
-    function search(element) {
-        var children = element.children;
-        for (var i = 0; i < children.length; i++) {
-            if (Array.prototype.indexOf.call(children[i].classList, className) > -1) {
-                result.push(children[i]);
-            }
-            if (children[i].childNodes && children[i].childNodes.length > 0) {
-                search(children[i]);
-            }
-        }
+// storage
+var result = [];
+// top element document.body
+var hasClass = function(element, className){
+    if (element.className === undefined) {
+        return false;
     }
-    search(document);
-    return result;
+    var elementClass = Array.prototype.slice.call(element.classList);
+    if (elementClass.indexOf(className) >= 0) {
+        return true;
+    }
+}
+
+// func search(element, className)
+    // check if the elem. has the className -> save to stra
+    // if children, iterate over children and recursivly
+var searchClass = function(element){
+    if (hasClass(element, className)) {
+        result.push(element);
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+        searchClass(element.childNodes[i]);
+    }
 };
+
+
+
+// searchClass(top element)
+searchClass(document.body);
+// return storage
+return result;
+};
+
+
